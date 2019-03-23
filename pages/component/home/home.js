df1f9738fd619ada6424e47b4afef5b621a10f33
lonflyone <2290843212@qq.com>
2019-03-23 19:32:52 +0800

    Signed-off-by: lonflyone <2290843212@qq.com>

--- pages/component/home/home.js
+++ pages/component/home/home.js
@@ -20,10 +20,32 @@ Component({
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
-    duration: 1000
-
+    duration: 1000,
+    day:'13',
+    month:'APRIL',
+    year:'2019',
   },
-
+  ready: function () {
+    var timestamp = Date.parse(new Date());
+     var date = new Date(timestamp);//获取年份  
+     var Y =date.getFullYear();//获取月份 
+      var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1);//获取当日日期 
+      var D = date.getDate() < 10 ? '0' + date.getDate() :date.getDate();
+      var chn_mon_arr = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']; 
+    var current_month = M;
+    var en_mon_arr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Spt", "Oct", "Nov", "Dec"];
+    for (var i = 0; i < chn_mon_arr.length; i++) {      //循环匹配       
+         if(current_month == chn_mon_arr[i]){       
+           M = en_mon_arr[i];         
+              }      
+          }
+this.setData({
+year:Y,
+month:M,
+day:D,
+})
+       
+  },
   /**
    * 组件的方法列表
    */
