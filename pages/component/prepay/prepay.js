2ec5b871332b1c8c9f946e7969000c917ef7812c
lonflyone <2290843212@qq.com>
2019-03-24 20:20:27 +0800

    Signed-off-by: lonflyone <2290843212@qq.com>

---
+++ pages/component/prepay/prepay.js
@@ -0,0 +1,28 @@
+// pages/component/prepay/prepay.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+   pai:1,
+   n1:997,
+   n2:91.5,
+   url:'/static/g1.svg',
+   price:2750,
+   preprice:2800,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
