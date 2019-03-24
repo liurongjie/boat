1a3015cc14331525f40541d5e645c4bb560680a6
likun_whu <1761834475@qq.com>
2019-03-24 17:29:39 +0800

    secondP的动画

--- pages/component/boat/boat.js
+++ pages/component/boat/boat.js
@@ -11,9 +11,14 @@ Component({
   /**
    */
   data: {
+   
+    lastX: 0,
+    lastY: 0,
+
 
   },
 
+  
   /**
    * 组件的方法列表
    */
@@ -25,5 +30,72 @@ Component({
       })
       console.log("gotoboat");
     },
-  }
+
+    swiper:function(e){
+      // console.log(e)
+      let currentX = e.changedTouches[0].pageX
+      let currentY = e.changedTouches[0].pageY
+
+      let tx =currentX -this.data.lastX
+      let ty = currentY - this.data.lastY
+
+      let text =""
+
+      if (Math.abs(tx) > Math.abs(ty)) {
+
+        //左右方向滑动
+
+        if (tx < 0)
+
+          text = "向左滑动"
+
+        else if (tx > 0)
+
+          text = "向右滑动"
+
+      }
+
+      else {
+
+        //上下方向滑动
+
+        if (ty < 0)
+
+          text = "向上滑动"
+
+        else if (ty > 0)
+
+          text = "向下滑动"
+
+      }
+
+
+
+      //将当前坐标进行保存以进行下一次计算
+
+      this.data.lastX = currentX
+
+      this.data.lastY = currentY
+
+      this.setData({
+
+        text: text,
+
+      })
+
+      console.log(text)
+      wx.showToast({
+        title: this.data.text,
+      })
+
+ 
+    },
+    
+  },
+
+ 
+  
+
+
+  
 })
