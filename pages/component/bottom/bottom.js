8fec4135b0786912fa250e6b937374565606ba8e
yongchun weng <2016301750020@whu.edu.cn>
2019-03-24 16:51:09 +0800

    3-24-1650

--- pages/component/bottom/bottom.js
+++ pages/component/bottom/bottom.js
@@ -4,64 +4,71 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
      //根据页面决定按钮执行的任务
       taskfunc:{
         type: String,
         value: ""
     },//任务调用的函数名
-=======
-
-
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
-  },
-
+     
+   },
 
   /**
    * 组件的方法列表
    */
 
   methods: {
-
+    // change1: function () {
+    //   const app = getApp();
+    //   app.index = 1;
+    //   this.setData({
+    //     index: 1,
+    //   })
+    // },
+    // change2: function () {
+    //   const app = getApp();
+    //   app.index = 2;
+    //   this.setData({
+    //     index: 2,
+    //   })
+    // },
+    // change3: function () {
+    //   const app = getApp();
+    //   app.index = 3;
+    //   this.setData({
+    //     index: 3,
+    //   })
+    // },
     goback:function(){
-       wx.navigateBack({
-       delta:1,
-       })
+      wx.navigateBack({
+        delta:1,
+      })
       //console.log(this.data.taskfunc);
     },
 
     goboat:function() {
       // body...
-      console.log("go to boat");
-    }
-
-
-    change1: function () {
-      const app = getApp();
-      app.index = 1;
-      this.setData({
-        index: 1,
-      })
+      console.log("go on boat");
     },
-    change2: function () {
-      const app = getApp();
-      app.index = 2;
-      this.setData({
-        index: 2,
-      })
+
+    myboat: function () {
+      // body...
+      console.log("see my boat");
     },
-    change3: function () {
-      const app = getApp();
-      app.index = 3;
-      this.setData({
-        index: 3,
-      })
+
+    prepay1: function () {
+      // body...
+      console.log("pre-pay 1 yuan");
+    },
+
+    prepay2: function () {
+      // body...
+      console.log("pre-pay half price");
     },
 
   }
