031655a7ba8dcf7708d847bcfb61c95c784931cb
yongchun weng <2016301750020@whu.edu.cn>
2019-03-24 12:59:45 +0800

    upload by vacutice

--- pages/component/boat/boat.js
+++ pages/component/boat/boat.js
@@ -1,4 +1,5 @@
 // pages/component/boat/boat.js
+var app=getApp();
 Component({
   /**
    * 组件的属性列表
@@ -18,6 +19,12 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    gotoboat: function () {
+      app.index = 4;//4代表参团页
+      wx.navigateTo({
+        url: '../toboat/toboat',
+      })
+      console.log("gotoboat");
+    },
   }
 })
