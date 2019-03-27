6fdc89af1c3ecd2cf89fb3ee507ecba23db6e63a
longchangfei <2290843212@qq.com>
2019-03-27 19:15:10 +0800

    Signed-off-by: longchangfei <2290843212@qq.com>

---
+++ pages/component/loading/loading.js
@@ -0,0 +1,27 @@
+// pages/component/loading/loading.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    visible: {//loading效果是否显示
+      type: Boolean,
+      value: false//默认不显示
+    },
+  },
+
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
