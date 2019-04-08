dafc421f1b0df91c2d71220b3d51b31fea8fbb0b
likun_whu <1761834475@qq.com>
2019-04-09 04:47:32 +0800

    home，产品详情页，地图，评论，预付1元------逻辑数据打通

--- pages/component/prepay/prepay.js
+++ pages/component/prepay/prepay.js
@@ -1,4 +1,7 @@
 // pages/component/prepay/prepay.js
+
+var common = require("../../../common/index.js")
+
 Component({
   /**
    * 组件的属性列表
@@ -11,12 +14,12 @@ Component({
    * 组件的初始数据
    */
   data: {
-   pai:1,
-   n1:997,
-   n2:91.5,
-   url:'/static/g1.svg',
-   price:2750,
-   preprice:2800,
+   data_list:{},
+    start_time: '',
+    end_time: '',
+    now: '',
+    date_day: "",
+    pic_url:'',
   },
 
   /**
@@ -24,5 +27,34 @@ Component({
    */
   methods: {
 
+  },
+
+  ready:function(){
+    this.setData({
+      data_list: common.currentData,
+      start_time: common.currentData.starttime,
+      end_time: common.currentData.endtime,
+    })
+
+    var timestamp = Date.parse(new Date());
+    timestamp = timestamp / 1000;
+    var temp;
+    temp = Math.ceil((timestamp - this.data.start_time) / (this.data.end_time - this.data.start_time) * 100);
+    var date = this.data.end_time - timestamp;
+    var date_day = parseInt(date / 3600 / 24);
+  
+    this.setData({
+      now: timestamp,
+      percent: temp,
+      date_day: date_day,
+     
+    })
+    console.log("[page4]获取当前时间戳为:" + timestamp); 
+
+
+    var url=this.data.data_list.production__introductionpic
+    this.setData({
+      pic_url:"https://xiaoyibang.top:8001/uploads/"+url
+    })
   }
 })
