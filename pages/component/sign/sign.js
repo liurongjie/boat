a68972340a285278ddca29784dbde83ea97bc36a
liurongjie <1397608894@qq.com>
2019-04-18 12:11:43 +0800

    sign

---
+++ pages/component/sign/sign.js
@@ -0,0 +1,26 @@
+// pages/component/sign/sign.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    sign:false,//是否完成签到
+    color:'gray',//button框颜色
+    text:'签到完成',//button框显示
+    date:1,//连续第几天
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
