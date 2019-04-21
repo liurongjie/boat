dfee66d38f9936fef774dfbf02a586ae3f35dd16
lonflyone <2290843212@qq.com>
2019-04-21 14:53:12 +0800

    Signed-off-by: lonflyone <2290843212@qq.com>

--- pages/component/asset/list3/list3.js
+++ pages/component/asset/list3/list3.js
@@ -11,13 +11,40 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    data:[
+      { id:'1',
+        pic: '/static/2.png',
+        name:'BOAT推荐驾校C1普通班 ',
+        price:'2029.5',
+        date: '2019/04/20',
+        erma:'/static/t14.png'
+      },
+      {
+        id: '2',
+        pic: '/static/3.png',
+        name: 'BOAT校园贝壳商城抽奖-实物奖品 ',
+        price: 'IPHONE-X',
+        date: '2019/04/20',
+        erma: '/static/t14.png'
+      },
+      {
+        id: '3',
+        pic: '/static/2.png',
+        name: 'BOAT推荐驾校C1普通班 ',
+        price: '2029.5',
+        date: '2019/04/20',
+        erma: '/static/t14.png'
+      },
+    ],
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    duihuan:function(e){
+      let query = e.currentTarget.dataset['index'];
+      console.log(query);
+    },
   }
 })
